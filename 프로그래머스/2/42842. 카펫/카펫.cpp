dab643ab38a472c53612cf7665b4ea14735f9538
김태후 <89252079+taehoo-kim@users.noreply.github.com>
2025-05-20 00:01:22 +0900

    [level 2] Title: 카펫, Time: 1.82 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+    
+    int flag = 0;
+    
+    for(int i = 1; i <= 5000; i++){
+        for(int j = 1; j <= i; j++){
+            if(2 * i + 2 * j - 4 == brown){
+                if(i * j - brown == yellow){
+                    answer.push_back(i);
+                    answer.push_back(j);
+                    flag = 1;
+                }
+            }
+        }
+        if(flag) break;
+    }
+
+    return answer;
+}
