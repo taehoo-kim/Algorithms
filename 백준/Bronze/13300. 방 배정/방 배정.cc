1c3dc6f455712a03fb6e5267545d7f9c407fa558
김태후 <89252079+taehoo-kim@users.noreply.github.com>
2025-08-25 22:39:40 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 2020 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#define MAX 1001
+
+using namespace std;
+
+void solution() {
+
+	int N, K;
+
+	cin >> N >> K;
+
+	int arr[2][7];
+	int rooms = 0;
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 7; j++) {
+			arr[i][j] = 0;
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		int S, Y;
+		cin >> S >> Y;
+		arr[S][Y]++;
+	}
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 1; j < 7; j++) {
+			if (arr[i][j] > K) {
+				rooms += arr[i][j] / K + 1;
+			}
+			else if (arr[i][j] == 0) continue;
+			else {
+				rooms++;
+			}
+		}
+	}
+
+	cout << rooms;
+
+}
+
+int main() {
+	
+	solution();
+	
+	return 0;
+}
